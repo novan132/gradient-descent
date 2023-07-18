3d3406e74f02a4ea6ef3f51acbdb75be0d8056bf
Novan <l4novan132@gmail.com>
2023-07-18 18:37:34 +0700

    feat: define function to compute gradient

--- qbGradient.cpp
+++ qbGradient.cpp
@@ -38,3 +38,33 @@ void qbGradient::SetMaxIterations(int maxIterations) {
 void qbGradient::SetGradientThresh(double gradientThresh) {
     m_gradientThresh = gradientThresh;
 }
+
+bool qbGradient::Optimize(std::vector<double> *funcLoc, double *funcVal) {
+    m_currentPoint = m_startPoint;
+}
+
+double qbGradient::ComputeGradient(int dim) {
+    std::vector<double> newPoint = m_currentPoint;
+    newPoint[dim] += m_h;
+
+    double funcVal1 = m_objectFcn(&m_currentPoint);
+    double funcVal2 = m_objectFcn(&newPoint);
+
+    return (funcVal2 - funcVal1) / m_h;
+}
+
+std::vector<double> qbGradient::ComputeGradientVector() {
+    std::vector<double> gradientVector = m_currentPoint;
+    for (int i = 0; i < m_nDims; ++i) {
+        gradientVector[i] = ComputeGradient(i);
+    }
+    return gradientVector;
+}
+
+double qbGradient::ComputeGradientMagnitude(std::vector<double> gradientVector) {
+    double vectorMagnitude = 0.0;
+    for (int i = 0; i < m_nDims; ++i) {
+        vectorMagnitude += gradientVector[i] * gradientVector[i];
+    }
+    return sqrt(vectorMagnitude);
+}
